87c6ada77fc5e2e629b53631572152d45ea78090
Oladunjoye Victor Xpan <xpanvictor@gmail.com>
2022-04-18 20:13:53 +0100

    Move

--- public/index.js
+++ public/index.js
@@ -1,12 +1,42 @@
 
-let videoHome = document.querySelector('#video')
 
+// Function to mute video
+function mute(){
+    let video = document.querySelector('video')
+    if (video.muted){
+        return video.muted = false
+    }
+    video.muted = true
+}
+
+let videoHome = document.querySelector('#video')
+let videoHome1 = document.querySelector('#video1')
+
+// Socket.io connection *********
+
+const socket = io('/')
+socket.emit('a_peer', 1)
+
+
+// ****** Socket.io end of the connection[tenuos]
+
+// The section for peer connection******
+const peer_me = new SimplePeer()
+
+// *****Peer connection end
+
+// Get user's camera first and wait for another user to connect.
 navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: {facingMode: "user", width: 600, height: 600}
-}).then((stream) => {
-    let video = document.createElement("video")
-    video.muted = false
+    video: {facingMode: "user", width: 350, height: 350}
+}).then(stream => {
+    let video = document.createElement('video')
+    video.muted = true
+    // Use socket to detect when a user has entered the chat and call
+    socket.on('new_peer', id => {
+        console.log("I'm now calling user", id)
+    })
+    
     addVideo(stream, video)
 })
 
@@ -17,3 +47,7 @@ function addVideo(stream, video){
     })
     videoHome.append(video)
 }
+
+window.addEventListener('beforeunload', ()=>{
+    socket.close()
+})
--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 const express = require('express')
 const path = require('path')
 
+// Server creation
 const app = new express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
-
+// serving dist files to the backend
 app.use(express.static(path.join(__dirname, '/public')))
-app.use(express.static(path.join(__dirname,'/node_modules/peerjs')))
+app.use(express.static(path.join(__dirname, 'node_modules/simple-peer')))
+app.use(express.static('node_modules/socket.io/client-dist'))
 
-app.use('room', function(req, res){
+// User const management
+let user = 0
+function newUser(){
+    return user++
+}
+
+// The routes management. I'll end up with client side rendering.
+app.use('/room', function(req, res){
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.use('', function(req, res){
     res.status(301).redirect('/room')
 })
 
-
-io.on('connection', (client)=>{
-    client.on('event', ()=>console.log('hey'))
+// Sockets and its connections management
+io.on('connection', (socket)=>{
+    socket.on('a_peer', (id)=>{
+        socket.broadcast.emit('new_peer', id)
+        console.log(`User id: ${id}`)
+    })
 })
 
-app.use('/room', function(req, res){
-    res.sendFile(path.join(__dirname, '/index.html'))
+io.on('disconnect', ()=>{
+    console.log('Oh no, you have been disconnected!')
 })
 
 let port = 3000
